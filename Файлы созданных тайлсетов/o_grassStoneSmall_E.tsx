da09aba900b7c263a2d2e62580479b5633adf09f
Lunazavr <luna.san279@gmail.com>
2021-02-20 01:39:00 +0300

    20.02

---
+++ Файлы созданных тайлсетов/o_grassStoneSmall_E.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="o_grassStoneSmall_E" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="../../../../Program Files/Tiled/GraphicsKits/Graphics #5/o_grassStoneSmall_E.png" width="256" height="512"/>
+</tileset>
